ea4577ed2f45ebcd8899c8a2653c7b1ead706144
ursulaklinger <klinger@punkt.de>
2023-12-11 21:54:48 +0100

    Update SentryProductionExceptionHandler.php

--- Classes/Handler/SentryProductionExceptionHandler.php
+++ Classes/Handler/SentryProductionExceptionHandler.php
@@ -10,27 +10,50 @@ namespace PunktDe\PtSentryClient\Handler;
 
 use PunktDe\PtSentryClient\Client;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\ContentObject\AbstractContentObject;
-use TYPO3\CMS\Frontend\ContentObject\Exception\ProductionExceptionHandler;
+use TYPO3\CMS\Core\Error\ProductionExceptionHandler;
+use TYPO3\CMS\Core\Controller\ErrorPageController;
+use TYPO3\CMS\Core\Error\Http\AbstractClientErrorException;
 
 class SentryProductionExceptionHandler extends ProductionExceptionHandler
 {
     /**
-     * @param \Exception $exception
-     * @param AbstractContentObject|null $contentObject
-     * @param mixed[] $contentObjectConfiguration
-     * @return string
-     * @throws \Exception
+     * Echoes an exception for the web.
+     *
+     * @param \Throwable $exception The throwable object.
      */
-    public function handle(
-        \Exception $exception,
-        AbstractContentObject $contentObject = null,
-        $contentObjectConfiguration = []
-    ): string {
-
+    public function echoExceptionWeb(\Throwable $exception)
+    {
         /** @var Client $client */
         $client = GeneralUtility::makeInstance(Client::class);
         $client->captureException($exception);
-        return parent::handle($exception, $contentObject, $contentObjectConfiguration);
+
+        $this->sendStatusHeaders($exception);
+        $this->writeLogEntries($exception, self::CONTEXT_WEB);
+        echo GeneralUtility::makeInstance(ErrorPageController::class)->errorAction(
+            $this->getTitle($exception),
+            $this->getMessage($exception),
+            $this->discloseExceptionInformation($exception) ? $exception->getCode() : 0,
+            503
+        );
+    }
+
+    /**
+     * Echoes an exception for the command line.
+     *
+     * @param \Throwable $exception The throwable object.
+     */
+    public function echoExceptionCLI(\Throwable $exception)
+    {
+        /** @var Client $client */
+        $client = GeneralUtility::makeInstance(Client::class);
+        $client->captureException($exception);
+
+        $filePathAndName = $exception->getFile();
+        $exceptionCodeNumber = $exception->getCode() > 0 ? '#' . $exception->getCode() . ': ' : '';
+        $this->writeLogEntries($exception, self::CONTEXT_CLI);
+        echo LF . 'Uncaught TYPO3 Exception ' . $exceptionCodeNumber . $exception->getMessage() . LF;
+        echo 'thrown in file ' . $filePathAndName . LF;
+        echo 'in line ' . $exception->getLine() . LF . LF;
+        die(1);
     }
 }
